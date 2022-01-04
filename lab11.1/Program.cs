5276e5b63cf1bb9f1577c166d2d852f07ebe7800
chemaeva8898@gmail.com <chemaeva8898@gmail.com>
2022-01-05 09:39:59 +1000

    Добавьте файлы проекта.

---
+++ lab11.1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab11._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите b:");
+            double b = Convert.ToDouble(Console.ReadLine());
+            double x = 0;
+            Console.WriteLine("Введите k:");
+            double k = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите y:");
+            double y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("{0} = {1}x + {2}", y, k, b);
+            Equation equation = new Equation { b = b, x = x, Ratio = k, addition = y };
+            // equation.Equation();
+            equation.Read(b, k, y);
+
+        }
+        struct Equation
+        {
+
+            public double b;
+            public double x;
+            public double Ratio; // коофицент К
+            public double addition; //сложение
+
+
+
+            public void Read(double b, double Ratio, double addition)
+            {
+
+                root();
+            }
+
+            public void root()
+            {
+                if (Ratio != 0) x = (addition - b) / Ratio;
+                Display();
+            }
+            public void Display()
+            {
+                Console.WriteLine("Решение x : {0} ", x);
+                Console.ReadKey();
+            }
+        }
+    }
+}
